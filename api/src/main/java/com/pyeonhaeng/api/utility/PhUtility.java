5a937a9563788a7aa3f8bb086092ed669d38978e
sungkyulh <sungkyulh@naver.com>
2023-04-05 23:23:41 +0900

    List를 json으로 변환하는 util 추가

--- api/src/main/java/com/pyeonhaeng/api/utility/PhUtility.java
+++ api/src/main/java/com/pyeonhaeng/api/utility/PhUtility.java
@@ -1,5 +1,11 @@
 package com.pyeonhaeng.api.utility;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pyeonhaeng.api.entity.ItemEntity;
+
+import java.util.List;
+import java.util.Objects;
+
 public final class PhUtility {
     private PhUtility(){}
 
@@ -7,5 +13,16 @@ public final class PhUtility {
     public static String checkName(String name){
         name = name.replaceAll("[\'\"']", "");
         return name;
+    }
+
+    public static String entity2Json(List<ItemEntity> entity){
+        ObjectMapper mapper = new ObjectMapper();
+        String json = new String();
+        try{
+            json = mapper.writeValueAsString(entity);
+        }catch (Exception e){
+            //TODO: 에러 처리
+        }
+        return json;
     }
 }
